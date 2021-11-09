990dd8c6625fad75aa3354c7b5db649f2ec79999
ahmadjlallad <ahmadjlad@hotmail.com>
2021-11-09 19:28:32 +0200

    8 yarb

---
+++ src/models/data-collection.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// THIS IS THE STRETCH GOAL ...
+// It takes in a schema in the constructor and uses that instead of every collection
+// being the same and requiring their own schema. That's not very DRY!
+
+class DataCollection {
+  constructor(model) {
+    this.model = model;
+  }
+
+  get(id) {
+    if (id) {
+      return this.model.findOne({ where: { id } });
+    } else {
+      return this.model.findAll();
+    }
+  }
+
+  create(record) {
+    return this.model.create(record);
+  }
+
+  async update(id, data) {
+    try {
+      const record = await this.model.findOne({ where: { id } });
+      if (!record) {
+        throw new Error("Record not found");
+      } else {
+        return await record.update(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  delete(id) {
+    return this.model.destroy({ where: { id } });
+  }
+}
+
+module.exports = DataCollection;
